850687e0a04983d1305c684da9d346e5482ee742
Joseph-Ten <85390890+Joseph-Ten@users.noreply.github.com>
2021-08-14 13:04:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,174 +1,257 @@
-var gameMode = "init";
-var player_cards = [];
-var computer_cards = [];
-var myOutputValue = "";
+// gamemodes
+var gameMode = "dealing cards";
 
-// ===== Making a deck of cards =======//
+// global state for some booleans
+
+var cardsFinished = false;
+
+// creating deck automatically using loop
+
 var makeDeck = function () {
-  var cardDeck = [];
-  var suits = ["hearts", "diamonds", "clubs", "spades"];
+  var deck = [];
 
-  // Loop over the suits array
+  // loop 1: separate to suits
+
   var suitIndex = 0;
+  var suits = ["hearts", "diamonds", "clubs", "spade"];
   while (suitIndex < suits.length) {
     var currentSuit = suits[suitIndex];
+    console.log(`current suit: ${currentSuit}`);
+
+    //loop 2: separate to rank 1-13
 
     var rankCounter = 1;
     while (rankCounter <= 13) {
-      var valueCounter = rankCounter;
       var cardName = rankCounter;
+      var cardValue = rankCounter;
+
+      // special cases for 1 11 12 13
 
       if (cardName == 1) {
-        cardName = "ace";
+        cardName = "Ace";
+        cardValue = 1;
       } else if (cardName == 11) {
-        cardName = "jack";
-        valueCounter = 10;
+        cardName = "Jack";
+        cardValue = 10;
       } else if (cardName == 12) {
-        cardName = "queen";
-        valueCounter = 10;
+        cardName = "Queen";
+        cardValue = 10;
       } else if (cardName == 13) {
-        cardName = "king";
-        valueCounter = 10;
+        cardName = "King";
+        cardValue = 10;
       }
-      var card = {
+
+      // creating the object
+
+      var cardDeck = {
         name: cardName,
         suit: currentSuit,
         rank: rankCounter,
-        value: valueCounter,
+        value: cardValue,
       };
 
-      cardDeck.push(card);
+      console.log(`rank : ${rankCounter}`);
 
+      // creating the object array
+
+      deck.push(cardDeck);
       rankCounter += 1;
+      console.log(cardDeck);
     }
+
     suitIndex += 1;
   }
-  return cardDeck;
+
+  console.log("deck", deck);
+  return deck;
 };
 
-// ===== Randomise the cards index ====== //
-// Getting a random index
+// function to make the deck
+
+var cardDeck = makeDeck();
+
+// shuffling the deck
+
 var getRandomIndex = function (max) {
   return Math.floor(Math.random() * max);
 };
 
-// ===== Shuffling the deck of cards ====== //
 var shuffleCards = function (cardDeck) {
-  // Loop over the card deck array once
   var currentIndex = 0;
   while (currentIndex < cardDeck.length) {
+    // Select a random index in the deck
     var randomIndex = getRandomIndex(cardDeck.length);
+    // Select the card that corresponds to randomIndex
     var randomCard = cardDeck[randomIndex];
+    console.log("random card ", randomCard);
+    // Select the card that corresponds to currentIndex
     var currentCard = cardDeck[currentIndex];
+    console.log("random card ", currentCard);
+    // Swap positions of randomCard and currentCard in the deck
     cardDeck[currentIndex] = randomCard;
     cardDeck[randomIndex] = currentCard;
+    console.log("random card after swap", randomCard);
+    console.log("currentcard after swap", currentCard);
+    // Increment currentIndex
     currentIndex = currentIndex + 1;
   }
+
   return cardDeck;
 };
 
-// // // ===== Computer Hit or Stand ====== //
-// var COM_HIT_OR_STAND = function (computer_cards) {
-//   if (computer_cards.value < 21 && computer_cards.length != 5) {
-//     computer_cards.push(shuffledDeck.pop());
-//   } else if (computer_cards.value == 21 && computer_cards.length <= 5) {
-//     var finalScore = 0;
-//     var index = 0;
-//     while (index < computer_cards.length) {
-//       finalScore = finalScore + computer_cards[index].value;
-//       index = index + 1;
-//     }
-//     return finalScore;
-//   }
-// };
+// function to shuffle cards based on the card deck we made
 
-// ===== Checking winning conditions ====== //
-var winning_check = function (final_playerScore, computer_finalScore) {
-  if (
-    (final_playerScore > computer_finalScore && final_playerScore < 21) ||
-    computer_finalScore > 21
-  ) {
-    myOutputValue =
-      "Your score is " + final_playerScore + " Computer's score is " + computer_finalScore + " <br><br> You have won!";
-  } else if (
-    (computer_finalScore > final_playerScore && computer_finalScore < 21) ||
-    final_playerScore > 21
-  ) {
-    myOutputValue =
-      "Your score is " + final_playerScore + " Computer's score is " + computer_finalScore + " <br><br> You have lost..";
-  } else if (
-    (computer_finalScore > 21 && final_playerScore > 21) ||
-    computer_finalScore == final_playerScore
-  ) {
-    myOutputValue = "Your score is " + final_playerScore + " Computer's score is " + computer_finalScore + " <br><br> It's a draw! ";
-  }
-  return myOutputValue;
-};
+var shuffledDeck = shuffleCards(cardDeck);
+console.log("shuffled deck", shuffledDeck);
+
+
+
+//============= start main function ===================
 
 var main = function (input) {
-  // Initialize the start of the game
-  var shuffledDeck = shuffleCards(makeDeck());
-  if (gameMode == "init") {
-    computer_cards = [shuffledDeck.pop(), shuffledDeck.pop()];
-    console.log(computer_cards);
-    player_cards = [shuffledDeck.pop(), shuffledDeck.pop()];
-    console.log(player_cards);
+  // initialize output
 
-    var drawn_playerCard =
-      player_cards[0].name +
-      " of " +
-      player_cards[0].suit +
-      " & " +
-      player_cards[1].name +
-      " of " +
-      player_cards[1].suit;
+  var output = "";
 
-    myOutputValue =
-      " Hello Player. These are your cards: " +
-      drawn_playerCard +
-      " <br><br> Would you like to hit or stand?";
-    gameMode = "player_hit and stand";
-  }
+  //pop takes the card at the end of the array and removes
 
-  if (computer_cards.value < 21 && computer_cards.length != 5) {
-    computer_cards.push(shuffledDeck.pop());
-  } else if (computer_cards.value == 21 && computer_cards.length <= 5) {
-    var computer_finalScore = 0;
-    var index = 0;
-    while (index < computer_cards.length) {
-      computer_finalScore = computer_finalScore + computer_cards[index].value;
-      index = index + 1;
-      console.log(computer_finalScore);
+  var computerCard1 = shuffledDeck.pop();
+  console.log("computer card 1", computerCard1);
+  var computerCard2 = shuffledDeck.pop();
+  console.log("computer card 2", computerCard2);
+  var playerCard1 = shuffledDeck.pop();
+  console.log("player card 1", playerCard1);
+  var playerCard2 = shuffledDeck.pop();
+  console.log("player card 2", playerCard2);
+  var playerCard3 = shuffledDeck.pop();
+  console.log("player card 3", playerCard3);
+
+  // function to collect computer score
+
+  var computerScore = function () {
+    totalComputerCardsScore = computerCard1.value + computerCard2.value;
+    return totalComputerCardsScore;
+  };
+  var playerScore = function () {
+    totalPlayerCardsScore = playerCard1.value + playerCard2.value;
+    return totalPlayerCardsScore;
+  };
+
+  var playerScore2 = function () {
+    totalPlayerCardsScore = playerScore() + playerCard3.value;
+    return totalPlayerCardsScore;
+  };
+
+  // DEALING CARDS
+
+  if (gameMode == "dealing cards") {
+    messageCompareResults = `Click submit to see who wins!`;
+    messageHitStand = `<br><br>Would you like to hit or stand? Type your choice in and press submit!`;
+    messageLess17 = `<br><br>You need to draw again as your score is less than 17. Type 'Hit' to add a card!`;
+    messageShuffledDeck = `The computer drew ${computerCard1.name} of ${
+      computerCard1.suit
+    } and ${computerCard2.name} of ${computerCard2.suit} .<br>You drew ${
+      playerCard1.name
+    } of ${playerCard1.suit} and ${playerCard2.name} of ${
+      playerCard2.suit
+    }.<br><br>The computer scored ${computerScore()}, while you scored ${playerScore()}. `;
+
+    output = messageShuffledDeck + messageHitStand;
+    gameMode = "hit or stand";
+
+    if (playerScore() < 17) {
+      output = messageShuffledDeck + messageLess17;
+      gameMode = "hit or stand";
+      return output;
     }
+
+    return output;
   }
 
-  if (gameMode == "player_hit and stand") {
-    if (input.toLowerCase() == "hit" && player_cards.length != 5) {
-      var new_card = shuffledDeck.pop();
-      player_cards.push(new_card);
-      myOutputValue =
-        " You have drawn " + new_card.name + " of " + new_card.suit;
-    } else if (input.toLowerCase() == "stand" || player_cards.length == 5) {
-      var counter = 0;
-      // Loop over the player cards array to check the total score of the cards
-      var final_playerScore = 0;
-      while (counter < player_cards.length) {
-        final_playerScore = final_playerScore + player_cards[counter].value;
-        counter = counter + 1;
-        console.log(final_playerScore);
-        gameMode = "score check";
+  if (gameMode == "hit or stand") {
+    if (input == "hit" || input == "Hit") {
+      messageHitScore = `You drew ${playerCard3.name} of ${
+        playerCard3.suit
+      }.<br><br>This brings your score to ${playerScore2()}, while the computer scored ${computerScore()}. `;
+
+      if (playerScore2() < 17) {
+        output = messageHitScore + messageLess17;
+        gameMode == "hit or stand";
+        return output;
+      } else if (playerScore2() >= 17 && playerScore2() <= 21) {
+        output = `${messageHitScore} ${messageCompareResults}`;
+        gameMode = "compare results";
+        return output;
+      } else if (playerScore2() > 21) {
+        output = `Bummer, you busted! You drew ${playerCard3.name} of ${
+          playerCard3.suit
+        }.<br><br>Your final score is ${playerScore2()} while the computer scored ${computerScore()}. Press submit or refresh the page to play again!`;
+        gameMode = "dealing cards";
+        return output;
       }
+    } else if (input == "stand" || input == "Stand") {
+      output = messageCompareResults;
+      gameMode = "compare results";
+      return output;
     }
   }
-  if (gameMode == "score check") {
-    myOutputValue = winning_check(final_playerScore, computer_finalScore);
+
+  // compare results - highest card wins
+  else if (gameMode == "compare results") {
+    console.log(`your compare results is working`);
+    if (computerScore() > playerScore() || computerScore() > playerScore2()) {
+      output = `You've lost. The computer won! Press submit or refresh the page to play again!`;
+      gameMode = "dealing cards";
+
+      return output;
+    } else if (
+      playerScore() > computerScore() ||
+      playerScore2() > computerScore()
+    ) {
+      output = `You beat the Computer! Press submit or refresh the page to play again!`;
+      gameMode = "dealing cards";
+      return output;
+    } else if (
+      playerScore() == computerScore() ||
+      playerScore2() == computerScore()
+    ) {
+      output = `It's a tie! Press submit or refresh the page to play again!`;
+      gameMode = "dealing cards";
+      return output;
+    }
   }
-  return myOutputValue;
+
+  // if all the cards are gone
+  // if the players hands are empty
+
+  if (shuffledDeck.length == 0 || shuffledDeck.length == 1) {
+    cardsFinished = true;
+    console.log("cards are finished");
+    output = "The deck is finished, refresh to start again";
+  }
 };
 
-// } // For the computer to get the closest number to 21
-//       if (gameMode == "computer_hit and stand") {
-//         var computer_finalScore = COM_HIT_OR_STAND(computer_cards);
-//         console.log(computer_finalScore);
-//         gameMode = "score check";
-//       }
+// // compare results - highest card wins
+// else if (gameMode == "compare results") {
+//   console.log(`your compare results is working`);
+//   if (computerScore() > playerScore() || computerScore() > playerScore2()) {
+//     output = `The computer won! The computer scored ${computerScore()}, while you scored ${playerScore2()}. Press submit or refresh the page to play again!`;
+//     gameMode = "dealing cards";
+
+//     return output;
+//   } else if (
+//     playerScore() > computerScore() ||
+//     playerScore2() > computerScore()
+//   ) {
+//     output = `You win! The computer scored ${computerScore()}, while you scored ${playerScore2()}. Press submit or refresh the page to play again!`;
+//     gameMode = "dealing cards";
+//     return output;
+//   } else if (
+//     playerScore() == computerScore() ||
+//     playerScore2() == computerScore()
+//   ) {
+//     output = `It's a tie! The computer scored ${computerScore()}, while you scored ${playerScore2()}. Press submit or refresh the page to play again!`;
+//     gameMode = "dealing cards";
+//     return output;
+//   }
+// }
